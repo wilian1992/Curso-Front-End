1c042a7f5e3476fd1421762ad565f276367d3f13
oiuuoiuiououiouiououuoi <wilianpaiva11@gmail.com>
2022-06-15 05:03:13 -0300

    branche javascript-3

--- javascript/README.md
+++ javascript/README.md
@@ -25,8 +25,8 @@
 
 ## Lógica de Programação
  - Computador:
-   -- Máquina que extrai dados
-   -- Processar: realizar operações nos dados de entrada
+ - - Máquina que extrai dados
+ - - Processar: realizar operações nos dados de entrada
  - Dado: é o que pode ser processado
  - Informação: resultado do processamento
  - Processamento de dados: Entrada (dados) > Processamento > Saída (informação)
--- javascript/js/script.js
+++ javascript/js/script.js
@@ -39,33 +39,125 @@ function contagemRegressiva(numero){
 
 // contagemRegressiva(50);
 
-document.addEventListener('submit', function( evento ){
+/* 
+ * Formulário envio de dados para cálculo da média 
+ */
+const formulario1 = document.getElementById('formulario-01');
 
-    evento.preventDefault();
-    evento.stopPropagation();
+if(formulario1)
+    formulario1.addEventListener('submit', function( evento ){
 
-    let formulario = document.getElementById('formulario-01');
+        evento.preventDefault();
+        evento.stopPropagation();
 
-    let dados = new FormData(formulario);
+        if( this.getAttribute('class').match(/erro/) ) {
+            return false;
+        }
+        
+        let dados = new FormData(this);
 
-    let objeto = {};
+        let notas = [];
 
-    let notas = [];
+        for(let key of dados.keys()) {
 
-    for(let key of dados.keys()) {
-        objeto[key] = dados.get(key);
+            let numero = dados.get(key).match(/\d*/) ? Number(dados.get(key)) : 0; // é um número
 
-        // adicionar itens no array
-        notas.push( parseInt(dados.get(key)));
+            if(!isNaN(numero)) {
+                notas.push(numero);
+            }
 
-    }
+        }
 
-    console.log(notas);
+        console.log(notas);
 
-    console.log(objeto);
+        texto = aprovacao(notas)
 
-    texto = aprovacao(notas)
+        document.getElementById('resultado').innerHTML = texto;
 
-    document.getElementById('resultado').innerHTML = texto;
+    });
 
-});
+
+function validaCampo(elemento){
+
+    elemento.addEventListener('focusout', function(event) {
+
+        event.preventDefault();
+
+        if(this.value == ""){
+            document.querySelector('.mensagem').innerHTML = "verifique o preenchimento dos campos em vermelho";
+            this.classList.add('erro');
+            this.parentNode.classList.add('erro');
+            return false;
+        } else {
+            document.querySelector('.mensagem').innerHTML = "";
+            this.classList.remove('erro');
+            this.parentNode.classList.remove('erro');
+        }
+
+    });
+
+}
+
+function validaCampoNumerico(elemento){
+
+    elemento.addEventListener('focusout', function(event) {
+
+        event.preventDefault();
+
+        let numero = this.value.match(/^[\d]5-[\d]3/) ? this.value.replace(/-/, "") : this.value; 
+
+        if(numero != "" && numero.match(/[0-9]*/) && numero >= 0 && numero <= 10){
+            document.querySelector('.mensagem').innerHTML = "";
+            this.classList.remove('erro');
+            this.parentNode.classList.remove('erro');
+        } else {
+            document.querySelector('.mensagem').innerHTML = "verifique o preenchimento dos campos em destaque";
+            this.classList.add('erro');
+            this.parentNode.classList.add('erro');
+            return false;
+        }
+
+    });
+
+}
+
+
+function validaEmail(elemento){
+
+    elemento.addEventListener('focusout', function(event) {
+
+        event.preventDefault();
+
+        const emailValido = /^[a-z0-9.]+@[a-z0-9]+\.[a-z]+(\.[a-z]+)?/i;
+        if(this.value.match(emailValido)) {
+            document.querySelector('.mensagem').innerHTML = "";
+            this.classList.remove('erro');
+            this.parentNode.classList.remove('erro');
+        } else {
+            document.querySelector('.mensagem').innerHTML = "verifique o preenchimento dos campos em destaque";
+            this.classList.add('erro');
+            this.parentNode.classList.add('erro');
+            return false;
+        }
+
+    });
+
+}
+
+
+let camposObrigatorios = document.querySelectorAll('input.obrigatorio');
+let camposNumericos = document.querySelectorAll('input.numero');
+let camposEmail = document.querySelectorAll('input.email');
+
+for( let emFoco of camposObrigatorios) {
+    validaCampo(emFoco);
+}
+
+for( let emFoco of camposNumericos) {
+    validaCampoNumerico(emFoco);
+}
+
+for( let emFoco of camposEmail) {
+    validaEmail(emFoco);
+}
+
